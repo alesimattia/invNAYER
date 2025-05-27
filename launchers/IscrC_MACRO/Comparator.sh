852d505bc55e4d101663e5062d8116873396dbb2
Alesi Mattia <alesimattia@gmail.com>
2025-05-27 12:40:49 +0200

    Calcolo metriche

--- launchers/IscrC_MACRO/Comparator.sh
+++ launchers/IscrC_MACRO/Comparator.sh
@@ -13,8 +13,8 @@
 python3 datafree_kd.py --workers 4 --gpu 0 --batch_size 512 --synthesis_batch_size 400 --lr 0.2 --warmup 20 \
  --epochs 1 --dataset cifar10 --method nayer --lr_g 4e-3 --teacher resnet34 --student resnet18 \
  --save_dir run/ --adv 1.33 --bn 10.0 --oh 0.5 --g_steps 30 --g_life 10 --g_loops 2 --gwp_loops 10 \
- --contr 0 --log_tag Compar_bestStud-scratch_PCA --tv_l2 0  --l2 0 --seed 32 \
+ --contr 0 --log_tag Compar_bestStud-scratch_PCA --seed 32 \
  --nayer_student ./checkpoints/datafree-nayer/cifar10-resnet34-resnet18--best_c10r34r18-tvL2-0.0005__l2-0.00001.pth \
  --scratch_student ./checkpoints/scratch/cifar10_resnet18_100ep.pth \
  --KD_student ./checkpoints/datafree-nayer/cifar10-resnet34-resnet18--KD_student_best_c10r34r18-tvL2-0.0005__l2-0.00001.pth \
- --metrics #All metrics
+ #Calcola tutte le metriche non specificando --metrics
