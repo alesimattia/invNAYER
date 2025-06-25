4b533fb6e38d60dc12c47b4f6ffcd1786a4805e1
Alesi Mattia <alesimattia@gmail.com>
2025-06-25 22:19:59 +0200

    Fix codecarbon

--- datafree_kd.py
+++ datafree_kd.py
@@ -397,7 +397,7 @@ def main():
     #print(result.stdout)
     if args.footprint:
         emissions: float = tracker.stop()
-        emissions /= 1000
+        emissions *= 1000
         print(f"Emissioni di carbonio: {emissions:.6f} gCO2eq")
         wandb.log({'Carbon emissions [gCO2eq]': emissions})
 
